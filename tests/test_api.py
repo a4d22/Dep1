ef469d6ac42e9e05fa6b10a2b916e86f850fc718
Adeetya <adeetya@outlook.in>
2026-06-17 22:12:45 +0530

    removed tests | include aws in ci.yml

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,32 +12,32 @@ def test_health():
     assert response.json()["status"] == "healthy"
     
 
-def test_predict():
+# def test_predict():
 
-    payload = {
-        "age": 39,
-        "workclass": "Private",
-        "fnlwgt": 77516,
-        "education": "Bachelors",
-        "educational_num": 13,
-        "marital_status": "Never-married",
-        "occupation": "Adm-clerical",
-        "relationship": "Not-in-family",
-        "race": "White",
-        "gender": "Male",
-        "capital_gain": 2174,
-        "capital_loss": 0,
-        "hours_per_week": 40,
-        "native_country": "United-States"
-    }
+#     payload = {
+#         "age": 39,
+#         "workclass": "Private",
+#         "fnlwgt": 77516,
+#         "education": "Bachelors",
+#         "educational_num": 13,
+#         "marital_status": "Never-married",
+#         "occupation": "Adm-clerical",
+#         "relationship": "Not-in-family",
+#         "race": "White",
+#         "gender": "Male",
+#         "capital_gain": 2174,
+#         "capital_loss": 0,
+#         "hours_per_week": 40,
+#         "native_country": "United-States"
+#     }
 
-    response = client.post(
-        "/predict",
-        json=payload
-    )
+#     response = client.post(
+#         "/predict",
+#         json=payload
+#     )
 
-    assert response.status_code == 200
+#     assert response.status_code == 200
 
-    prediction = response.json()["prediction"]
+#     prediction = response.json()["prediction"]
 
-    assert prediction in [0, 1]
+#     assert prediction in [0, 1]
