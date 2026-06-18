87d6e6b648d5e84c355b718287cfcbdd48b79a14
Adeetya <adeetya@outlook.in>
2026-06-18 06:55:36 +0530

    temp model pkl in image

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY serving ./serving
 
+COPY artifacts/model.pkl ./artifacts/model.pkl
+
 EXPOSE 8000
 
 CMD ["uvicorn", "serving.app:app", "--host", "0.0.0.0", "--port", "8000"]
--- serving/model_loader.py
+++ serving/model_loader.py
@@ -1,24 +1,39 @@
-import os
-import mlflow
+# import os
+# import mlflow
 
-mlflow.set_tracking_uri(
-    os.getenv(
-        "MLFLOW_TRACKING_URI",
-        "http://localhost:5000"
-    )
-)
+# mlflow.set_tracking_uri(
+#     os.getenv(
+#         "MLFLOW_TRACKING_URI",
+#         "http://localhost:5000"
+#     )
+# )
+
+# _model = None
+
+
+# def get_model():
+
+#     global _model
+
+#     if _model is None:
+
+#         _model = mlflow.pyfunc.load_model(
+#             "models:/IncomeModel@Champion"
+#         )
+
+#     return _model
+
+import joblib
 
 _model = None
-
 
 def get_model():
 
     global _model
 
     if _model is None:
-
-        _model = mlflow.pyfunc.load_model(
-            "models:/IncomeModel@Champion"
+        _model = joblib.load(
+            "artifacts/model.pkl"
         )
 
     return _model
